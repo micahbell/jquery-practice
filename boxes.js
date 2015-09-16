051ff17e6f462562bc025ee43a1cbf38bf355772
Micah Bell <micahmbell@gmail.com>
2015-09-16 13:04:21 -0600

    jQuery practice.

--- boxes.js
+++ boxes.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+$(document).ready(function() {
+  $('#secretBox').css('background-color', 'white').html('SecretBox');
+
+  $('#row1 > div').addClass('boxType3');
+
+  $('#row4 div:last-child').hide();
+
+  $('.boxType1').css('background-color', 'white');
+
+
+  // $('#row2 div:nth-child(-n+2)').remove('style');
+});
